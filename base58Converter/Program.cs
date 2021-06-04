5b7ec2c69b69c40f397ac8de7d56c56518140953
Dragan Pepic <d.pepic@hotmail.com>
2021-06-04 13:40:47 +0200

    Add project files.

---
+++ base58Converter/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Numerics;
+
+namespace base58Converter
+{
+	class Program
+	{
+		static string base58Charset = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz";
+		static string result = "";
+
+		static void Main(string[] args)
+		{
+			Encode(new BigInteger(123456789));
+			Console.WriteLine(result);
+			Console.WriteLine(Decode());
+			Console.ReadKey();
+		}
+
+		static void Encode(BigInteger t)
+		{
+			result = result.Insert(0, base58Charset[(int)t % 58].ToString());
+			if (t / 58 is BigInteger next && next != 0)
+				Encode(next);
+		}
+
+		static BigInteger Decode() => result.Reverse()
+											.Select((val, dex) => new Tuple<char, int>(val, dex))
+											.Aggregate(new BigInteger(), (agg, base58tuple) =>
+												  agg += base58Charset.IndexOf(base58tuple.Item1) * (int)Math.Pow(58, base58tuple.Item2));
+	}
+}
